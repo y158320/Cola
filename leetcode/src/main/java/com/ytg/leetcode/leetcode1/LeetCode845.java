8fcf1eb043edcba03a7cb9221487f50ad59294ad
yangtiangang <651236807@qq.com>
2020-10-25 17:30:32 +0800

    add some leetcode

---
+++ leetcode/src/main/java/com/ytg/leetcode/leetcode1/LeetCode845.java
@@ -0,0 +1,26 @@
+package com.ytg.leetcode.leetcode1;
+
+public class LeetCode845 {
+
+    public int longestMountain(int[] A) {
+        int before=0;
+        int end=0;
+        int i=0;
+        int j=0;
+        int count=0;
+        while(i<A.length-1)
+        {
+            j=i;
+            while (j<A.length-1)
+            {
+                if (A[j]<A[j+1])
+                {
+
+                }
+            }
+        }
+        return 0;
+
+    }
+
+}
